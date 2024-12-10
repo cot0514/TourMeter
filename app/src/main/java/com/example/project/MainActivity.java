c3f3b44f3283d0abdc375688d1df4f6c510ac1d3
[cot0514] <[cot0514@gmail.com]>
2024-12-10 17:01:52 +0900

    CSV 파일을 불러와 spinner에 데이터를 집어 넣기
    Signed-off-by: [cot0514] <[cot0514@gmail.com]>

--- app/src/main/java/com/example/project/MainActivity.java
+++ app/src/main/java/com/example/project/MainActivity.java
@@ -3,7 +3,12 @@ package com.example.project;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.TextView;
 
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
@@ -13,20 +18,28 @@ import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
 import com.google.gson.Gson;
 
-import androidx.activity.EdgeToEdge;
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+import com.opencsv.CSVReader;
 
+import androidx.appcompat.app.AppCompatActivity;
+
+import java.io.InputStreamReader;
 import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
     String key = "Y8du4%2B8y%2BfX2MHxWxQ63PKe5MiTdnI3Slf2J3tb0KgHK6uqDT5VzuIhae8pKxfm%2BR5gXUHatD4V7dQ0oDspWYg%3D%3D";
     String main = "http://apis.data.go.kr/1360000/TourStnInfoService1/getCityTourClmldx1";
 
-    RequestQueue queue = Volley.newRequestQueue(this);
+    EditText date;
+    EditText day;
+    EditText id;
+
+     static RequestQueue queue;
+
+    private List<SettingData> locations = new ArrayList<SettingData>();
+    private List<String> cityList = new ArrayList<String>();
+    private List<SettingData> selectedDistricts = new ArrayList<SettingData>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,11 +48,75 @@ public class MainActivity extends AppCompatActivity {
 
         UrlInfo info = new UrlInfo();
 
-        info.pageNo = findViewById(R.id.pageNo).toString();
-        info.numOfRows = findViewById(R.id.numOfRows).toString();
-        info.curDate = findViewById(R.id.curDate).toString();
-        info.hour = findViewById(R.id.hour).toString();
-        info.id = findViewById(R.id.id).toString();
+        date = findViewById(R.id.curDate);
+        day = findViewById(R.id.day);
+
+        Spinner spinnerCity = findViewById(R.id.city);
+        Spinner spinnerDistrict = findViewById(R.id.district);
+        TextView cityId = findViewById(R.id.id);
+
+        loadCSVData();
+
+        for (SettingData location : locations) {
+            if(!cityList.contains(location.getCity()))
+            {
+                cityList.add(location.getCity());
+            }
+        }
+
+        ArrayAdapter<String> cityAdapter = new ArrayAdapter<>(
+                this,
+                android.R.layout.simple_spinner_item,
+                cityList
+        );
+        cityAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinnerCity.setAdapter(cityAdapter);
+
+        spinnerCity.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, android.view.View view, int position, long id) {
+                String selectedCity = cityList.get(position);
+
+                selectedDistricts.clear();
+                for (SettingData location : locations) {
+                    if (location.getCity().equals(selectedCity)) {
+                        selectedDistricts.add(location);
+                    }
+                }
+
+                List<String> districtNames = new ArrayList<>();
+                for (SettingData location : selectedDistricts) {
+                    districtNames.add(location.getDistrict());
+                }
+
+                ArrayAdapter<String> districtAdapter = new ArrayAdapter<>(
+                        MainActivity.this,
+                        android.R.layout.simple_spinner_item,
+                        districtNames
+                );
+                districtAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+                spinnerDistrict.setAdapter(districtAdapter);
+
+                spinnerDistrict.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+                    @Override
+                    public void onItemSelected(AdapterView<?> parent, android.view.View view, int position, long id) {
+
+                        String selectedId = selectedDistricts.get(position).getId();
+                        cityId.setText(selectedId);
+                    }
+
+                    @Override
+                    public void onNothingSelected(AdapterView<?> parent) {}
+                });
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {}
+        });
+
+        info.curDate = date.getText().toString();
+        info.day = day.getText().toString();
+        info.id = cityId.getText().toString();
 
         Button button = findViewById(R.id.button);
 
@@ -49,17 +126,21 @@ public class MainActivity extends AppCompatActivity {
                 makeRequest(info);
             }
         });
+
+        if (queue == null) {
+            queue = Volley.newRequestQueue(getApplicationContext());
+        }
     }
 
     public void makeRequest(UrlInfo info) {
         String url = main +
                 "?ServiceKey=" + key +
-                "&PageNo=" + info.pageNo +
-                "&numOfRows=" + info.numOfRows +
+                "&PageNo=1" +
+                "&numOfRows=" + info.day +
                 "&dataType=JSON" +
                 "&CURRENT_DATE=" + info.curDate +
-                "&HOUR" + info.hour +
-                "&COURSE_ID" + info.id;
+                "&DAY=" + info.day +
+                "&CITY_AREA_ID=" + info.id;
 
         StringRequest stringRequest = new StringRequest(Request.Method.GET, url,
                 new Response.Listener<String>() {
@@ -83,6 +164,22 @@ public class MainActivity extends AppCompatActivity {
 
     public void processResponse(String response) {
         Gson gson = new Gson();
+        WeatherList weatherList = gson.fromJson(response, WeatherList.class);
 
+
+
+    }
+
+    private void loadCSVData() {
+        try (CSVReader reader = new CSVReader(new InputStreamReader(getAssets().open("data.csv")))) {
+            String[] nextLine;
+            while ((nextLine = reader.readNext()) != null) {
+                if (nextLine.length == 4) { // city, district, id
+                    locations.add(new SettingData(nextLine[0], nextLine[1], nextLine[2], nextLine[3]));
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
--- app/src/main/java/com/example/project/WeatherList.java
+++ app/src/main/java/com/example/project/WeatherList.java
@@ -1,5 +1,5 @@
 package com.example.project;
 
 public class WeatherList {
-    WeatherListResult ss;
+    WeatherListResult weatherList;
 }
