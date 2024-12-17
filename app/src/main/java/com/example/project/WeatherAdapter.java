4d3de50ddafaf826f6cdc0114f389a664ec2a27f
[cot0514] <[cot0514@gmail.com]>
2024-12-17 20:06:14 +0900

    Final version
    Signed-off-by: [cot0514] <[cot0514@gmail.com]>

--- app/src/main/java/com/example/project/WeatherAdapter.java
+++ app/src/main/java/com/example/project/WeatherAdapter.java
@@ -45,15 +45,19 @@ public class WeatherAdapter extends RecyclerView.Adapter<WeatherAdapter.WeatherV
         switch (item.TCI_GRADE) {
             case "매우 좋음":
                 holder.tci_grade.setImageResource(R.drawable.very_good);
+                holder.itemView.setBackgroundResource(R.drawable.sky_blue);
                 break;
             case "좋음":
                 holder.tci_grade.setImageResource(R.drawable.good);
+                holder.itemView.setBackgroundResource(R.drawable.green);
                 break;
             case "보통":
                 holder.tci_grade.setImageResource(R.drawable.normal);
+                holder.itemView.setBackgroundResource(R.drawable.yellow);
                 break;
             default:
                 holder.tci_grade.setImageResource(R.drawable.bad);
+                holder.itemView.setBackgroundResource(R.drawable.red);
                 break;
         }
     }
